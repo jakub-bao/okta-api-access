da792b69aed0a0439705d0aea06031d26823988f
Jakub <jakub.flaska@afs.com,jflaska@baosystems.com>
2024-12-06 14:45:02 -0500

    working

--- Makefile
+++ Makefile
@@ -4,10 +4,19 @@ validate:
 login:
 	aws sso login
 
-invoke:
+build:
 	sam build
-	cp  ~/SCRIPTS/aws/aws-sam/cacert.pem .aws-sam/build/GetByEmail/certifi
-	sam local invoke
 
-debug:
+deploy: build
+	sam deploy
+
+invoke: build
+	cp  ~/SCRIPTS/aws/aws-sam/cacert.pem .aws-sam/build/GetByEmail/certifi
 	sam local invoke --debug
+
+test:
+	aws lambda invoke --function-name okta-api-access-GetByEmail-7XFMQPSOJkiB test.out
+	cat test.out
+
+all: deploy test
+
--- code/getByEmail/app.py
+++ code/getByEmail/app.py
@@ -1,12 +1,37 @@
-import json
 import boto3
+import asyncio
 from okta.client import Client as OktaClient
-from okta.models import UserProfile, CreateUserRequest
 
 okta_key_ssm = 'external/api/okta_private_key'
 
+def get_okta_api_key():
+    private_key = boto3.client('secretsmanager').get_secret_value(SecretId=okta_key_ssm)['SecretString']
+    assert 'RSA PRIVATE KEY' in private_key
+    return private_key
+
+def get_okta_client(api_key):
+    config = {
+        'orgUrl': 'https://statedev.okta.com',
+        'clientId': '0oate4b172simvVHJ297',
+        'authorizationMode': 'PrivateKey',
+        'scopes': [
+            'okta.users.read',
+            'okta.users.manage',
+            'okta.groups.read',
+            'okta.groups.manage'
+        ],
+        'issuer': f'https://statedev.okta.com/oauth2/default',
+        'privateKey': api_key,
+    }
+    return OktaClient(config)
+
+async def list_groups(okta):
+    groups, resp, error = await okta.list_groups()
+    print(f'Okta response error: {error}')
+    return resp
 
 def lambda_handler(event, context):
-    private_key = boto3.client('secretsmanager').get_secret_value(SecretId=okta_key_ssm)['SecretString']
-    print(private_key)
-    return 'finished'
+    api_key = get_okta_api_key()
+    okta = get_okta_client(api_key)
+    resp = asyncio.run(list_groups(okta))
+    return resp.get_body()
